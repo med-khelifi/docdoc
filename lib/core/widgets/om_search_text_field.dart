0afa75f0781ef29ea1818eb57d38a4ced70eb410
Mohamed khelifi <khelifim440@gmail.com>
2026-07-05 15:18:57 +0100

    feat: implement CustomSearchTextField in SearchView with improved layout and padding

---
+++ lib/core/widgets/om_search_text_field.dart
@@ -0,0 +1,45 @@
+import 'package:doctorine/core/extensions/build_context_extensions.dart';
+import 'package:doctorine/core/helpers/spaces.dart';
+import 'package:doctorine/core/theme/app_colors.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CustomSearchTextField extends StatelessWidget {
+  const CustomSearchTextField(
+      {super.key, this.isFilterEnabled, this.onFilterPressed});
+  final bool? isFilterEnabled;
+  final VoidCallback? onFilterPressed;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: TextField(
+            style: context.textStyles.bodyMedium,
+            decoration: const InputDecoration(
+              hintText: 'Search...',
+              prefixIcon: Icon(
+                CupertinoIcons.search,
+                color: AppColors.grey,
+                size: 32,
+              ),
+            ),
+          ),
+        ),
+        if (isFilterEnabled ?? true) ...[
+          const HorizontalSpace(width: 8),
+          IconButton(
+              onPressed: onFilterPressed,
+              style: IconButton.styleFrom(
+                backgroundColor: AppColors.transparent,
+              ),
+              icon: Icon(
+                Icons.filter_list_outlined,
+                color: context.colorScheme.tertiary,
+                size: 32,
+              ))
+        ]
+      ],
+    );
+  }
+}
--- lib/features/search/views/search_view.dart
+++ lib/features/search/views/search_view.dart
@@ -1,4 +1,6 @@
 import 'package:doctorine/core/helpers/spaces.dart';
+import 'package:doctorine/core/widgets/custom_app_bar.dart';
+import 'package:doctorine/core/widgets/om_search_text_field.dart';
 import 'package:flutter/material.dart';
 
 class SearchView extends StatelessWidget {
@@ -7,20 +9,26 @@ class SearchView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          const VerticalSpace(height: 50),
-          Expanded(
-            child: ListView.builder(
-              itemCount: 10,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text('Item $index'),
-                );
-              },
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 12.0),
+        child: Column(
+          children: [
+            const VerticalSpace(height: 20),
+            const CustomAppBar(title: "search"),
+            const VerticalSpace(height: 25),
+            const CustomSearchTextField(),
+            Expanded(
+              child: ListView.builder(
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text('Item $index'),
+                  );
+                },
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
